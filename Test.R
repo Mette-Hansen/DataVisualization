b3d08f2bfbead599b796edb950c54cf58d5d3f58
Mette Hansen <meha419@student.sdu.dk>
2022-09-26 13:16:02 +0200

    Start exercises

---
+++ Test.R
@@ -0,0 +1,33 @@
+#Practice 1 
+((2022-2019)/(2022-1998))*100
+
+#Practice 2
+diff_study <- 2022-2019
+diff_born <- 2022-1998
+diff_study/diff_born*100
+
+#Practice 3
+#sum(4,5,8,11)
+x<-c(4,5,8,11)
+sum(x)
+x
+
+#Practice 4
+y<-rnorm(1000,10,1.4)
+
+#Practice 5
+plot(y)
+library(ggplot2)
+#ggplot(data = y, mapping = aes())
+
+#Practice 6
+?sqrt
+
+#Practice 7
+#See firstscript.R
+
+#Practice 8
+p <- c(31:60)
+#p<-seq(31,60,by=2)
+q <- matrix(p, nrow = 6, ncol = 5, byrow = TRUE)
+q
