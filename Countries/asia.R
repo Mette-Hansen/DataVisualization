2f0a20b8e534d7093a6e29ded007afa6688e2650
Mette Hansen <meha419@student.sdu.dk>
2022-10-03 15:09:14 +0200

    Shiney start

--- Countries/asia.R
+++ Countries/asia.R
@@ -6,3 +6,9 @@ ggplot(asia) + geom_line(mapping = aes(x = year, y = gdpPercap, group=country))
 ggplot(asia) + geom_line(mapping = aes(x = year, y = gdpPercap, color=country))
 # Dotted lines + colors
 ggplot(asia) + geom_line(mapping = aes(x = year, y = gdpPercap, linetype=country, color = country))
+
+library(gganimate)
+library(gapminder)
+
+library(shiny)
+runExample("01_hello")
