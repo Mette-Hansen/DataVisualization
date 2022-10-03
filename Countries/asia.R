2677826bf00abe936155deefba70e46044e4731d
Mette Hansen <meha419@student.sdu.dk>
2022-10-03 12:44:15 +0200

    Clean up

--- Countries/asia.R
+++ Countries/asia.R
@@ -4,3 +4,5 @@ asia <- read_excel("asia.xlsx")
 View(asia)
 ggplot(asia) + geom_line(mapping = aes(x = year, y = gdpPercap, group=country))
 ggplot(asia) + geom_line(mapping = aes(x = year, y = gdpPercap, color=country))
+# Dotted lines + colors
+ggplot(asia) + geom_line(mapping = aes(x = year, y = gdpPercap, linetype=country, color = country))
